10d6f56863b63d406807d96333d418be8a62d7d4
Projecttbp <156791374+Projecttbp@users.noreply.github.com>
2024-02-05 12:25:58 +0530

    Add files via upload

---
+++ crop_monitoring_project/crop_monitoring_app/models.py
@@ -0,0 +1,15 @@
+# crop_monitoring_app/models.py
+from django.db import models
+
+class Crop(models.Model):
+    name = models.CharField(max_length=255)
+
+class CropPrice(models.Model):
+    crop = models.ForeignKey(Crop, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+
+class Price(models.Model):
+    crop = models.ForeignKey(Crop, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
